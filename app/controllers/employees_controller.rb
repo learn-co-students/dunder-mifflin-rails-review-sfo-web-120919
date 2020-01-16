dc60ff71ab597412276658ccb17b66100bd8dee5
brookebachman00 <yourpetlover4411@yahoo.com>
2020-01-15 16:52:37 -0800

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,29 @@
 class EmployeesController < ApplicationController
+   
+    def index
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to employees_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  resources :dogs, only: [:show, :index]
+  resources :employees, only: [:new, :show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
