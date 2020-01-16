2b4bba003c88e1c67fa6e292f9b1d126350d8fa2
monkeypineapple <davidrosenholz@gmail.com>
2020-01-15 18:46:40 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  get 'dogs/index'
+
+  get 'dogs/showedit'
+
+  get 'dogs/new'
+
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
